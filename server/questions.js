b7506b8f38a408bd0457c17b9b4f4f0625de113c
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-08-20 16:39:42 +0200

    Changed questions api

--- server/questions.js
+++ server/questions.js
@@ -28,16 +28,8 @@ function getQuestions(gameOptions) {
     let finalGameOptions = {...defaultOptions, ...gameOptions};
     let rawdata = fs.readFileSync('./questions.json');
     let data = JSON.parse(rawdata);
-    data = data.filter(q => q.difficulty.toUpperCase() === finalGameOptions.difficulties.toUpperCase());
-    data = getRandomQuestions(data, finalGameOptions.questionsPerRound * finalGameOptions.numberOfRounds);
-
-    let mappedData = data.map(question => {
-        return {
-            question: question.question,
-            incorrectAnswers: getIncorrectAnswers(question.answers, question.correct_answers),
-            correctAnswer: getCorrectAnswer(question.answers, question.correct_answers)
-        }
-    });
+    console.log(data.length);
+    let mappedData = getRandomQuestions(data, finalGameOptions.questionsPerRound * finalGameOptions.numberOfRounds);
 
     return mappedData;
 }
@@ -45,7 +37,7 @@ function getQuestions(gameOptions) {
 function getRandomQuestions(questions, numQuestions) {
     var arr = [];
     while(arr.length < numQuestions){
-        var r = Math.floor(Math.random() * 100) + 1;
+        var r = Math.floor(Math.random() * (questions.length-1)) + 1;
         if(arr.indexOf(r) === -1) arr.push(r);
     }
     let randomQ = [];
@@ -53,58 +45,6 @@ function getRandomQuestions(questions, numQuestions) {
         randomQ.push(questions[i]);
     });
     return randomQ;
-}
-
-function getIncorrectAnswers(answers, correctArr){
-    let output = [];
-    if(correctArr.answer_a_correct == "false"){
-        output.push(answers.answer_a)
-    }
-    if(correctArr.answer_b_correct == "false"){
-        output.push(answers.answer_b)
-    }
-    if(answers.answer_c != null && correctArr.answer_c_correct == "false"){
-        output.push(answers.answer_c)
-    }
-    if(answers.answer_d != null && correctArr.answer_d_correct == "false"){
-        output.push(answers.answer_d)
-    }
-    if(answers.answer_e != null && correctArr.answer_e_correct == "false"){
-        output.push(answers.answer_e)
-    }
-    if(answers.answer_f != null && correctArr.answer_f_correct == "false"){
-        output.push(answers.answer_f)
-    }
-
-    if(output.length <= 3){
-        return output;
-    } else {
-        return output.slice(0, 3);
-    }
-    
-}
-
-function getCorrectAnswer(answers, correctArr){
-    if(correctArr.answer_a_correct == "true"){
-        return answers.answer_a;
-    } else
-    if(correctArr.answer_b_correct == "true"){
-        return answers.answer_b;
-    } else
-    if(answers.answer_c != null && correctArr.answer_c_correct == "true"){
-        return answers.answer_c;
-    } else
-    if(answers.answer_d != null && correctArr.answer_d_correct == "true"){
-        return answers.answer_d;
-    } else
-    if(answers.answer_e != null && correctArr.answer_e_correct == "true"){
-        return answers.answer_e;
-    } else
-    if(answers.answer_f != null && correctArr.answer_f_correct == "true"){
-        return answers.answer_f;
-    }
-
-    return "";
 }
 
 export { getQuestions};
