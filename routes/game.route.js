8d34f8c3d6894c3674613b294c0d3de83393b03c
Johan Smit <johansm@bbd.co.za>
2023-08-23 11:30:45 +0200

    Scoreboard fix?

--- routes/game.route.js
+++ routes/game.route.js
@@ -11,50 +11,31 @@ import { Router } from "express";
 const gameRouter = Router()
 
 gameRouter.get("/leaderboard", async (req, res) => {
-    let gameId = req.query.gameId;
-    let response = await getGameLeaderboardRequest(gameId);
-    response = await response.json();
+    try {
+      let gameId = req.query.gameId;
+      let response = await getGameLeaderboardRequest(gameId);
+      response = await response.json();
 
-    let results = await Promise.all(response.records.map(async score => {
+      let results = await Promise.all(response.records.map(async score => {
 
-        let user = await getUSerByID(score.fields.user_id);
-        user = await user.json();
-        user = user.records[0];
-        
-        return {
-            "name" : user.fields.first_name + " " + user.fields.last_name,
-            "score" : score.fields.score
-        }
-    }));
+          let user = await getUSerByID(score.fields.user_id);
+          user = await user.json();
+          user = user.records[0];
+          
+          return {
+              "name" : user.fields.first_name + " " + user.fields.last_name,
+              "score" : score.fields.score
+          }
+      }));
 
-    res.status(200).json({
-        leaderboard: JSON.stringify(results)
-    });
-
-    
-
-    // getGameLeaderboardRequest(gameId).then(async (leaderboard) => {
-        
-    //     leaderboard = await leaderboard.json();
-    //     console.log(leaderboard);
-
-    //     let results = leaderboard.map(score => {
-    //         return {
-    //             "name" : score.get("first_name"),
-    //             "score" : score.get("score")
-    //         }
-    //     })
-
-    //     let jsonMap = JSON.stringify(results);
-
-    //     res.status(200).json({
-    //         leaderboard: jsonMap
-    //     });
-    // }).catch((err) => {
-    //     console.log(err);
-    //     res.status(400);
-    //     res.send(err);
-    // })
+      res.status(200).json({
+          leaderboard: JSON.stringify(results)
+      });
+    } catch (err) {
+        console.log(err);
+        res.status(400);
+        res.send(err);
+    }
 });
   
 gameRouter.get("/leaderboard/all", async (req, res) => {
